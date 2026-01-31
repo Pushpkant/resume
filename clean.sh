d542a24d3818ca8a02db1677e9eb10a832957987
Pushpkant Yadav <pushpkantyadav@gmail.com>
2026-01-31 11:25:21 +0530

    Adding Resume format.

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -vrf *.gz
+rm -vrf *.aux
+rm -vrf *.bbl
+rm -vrf *.blg
+rm -vrf *.dvi
+rm -vrf *.glg
+rm -vrf *.glo
+rm -vrf *.gls
+rm -vrf *.ist
+rm -vrf *.log
+rm -vrf *.lol
+rm -vrf *.lot
+rm -vrf *.out
+#rm -vrf *.pdf
+rm -vrf *.toc
+rm -vrf *.lof
+
